01f150b6af8c69e051a1866d6b9f3fdfd376b74e
Krishna Kant Singh <76570668+KrishnaKant68531@users.noreply.github.com>
2022-02-06 18:59:20 +0530

    Add files via upload
    In this project we take a pdf file and with the help of python libraries Encrypt PDF file into password protected and also store the file details in a SQL database along with csv file as well.

--- PDF Creator.py
+++ PDF Creator.py
@@ -1,47 +1,37 @@
 import PyPDF2
-import os
 import tkinter
 import sqlite3
-import shutil
+import os
 import csv
 import math
 from datetime import datetime
-from datetime import timedelta
 from tkinter import filedialog
 root = tkinter.Tk()
 root.withdraw()
 
-global inputpdf
 
-
-class assign:
-    def time(self):
-        current_time = datetime.now()
-        print(current_time)
-        print(type(current_time))
-        modified_time = current_time.replace(microsecond=0)
-        print(modified_time)
-        added_current_time = modified_time + timedelta(minutes=330)
-        print(added_current_time)
-        my_time_format = "%Y_%m_%d_%H_%M_%S"
-        converted_format_time = datetime.strftime(added_current_time, my_time_format)
-        print(type(converted_format_time))
-        print(converted_format_time)
-        print()
+class Project:
 
     def pdf(self):
-        pdf_folder = r"C:\Users\Shreyas V Shetty\PycharmProjects\pythonProject\pythonProject\Pdf_Encryption\Pdf " \
-                     r"Encrypted files"
+        pdf_folder = r"C:\Users\Shreyas V Shetty\PycharmProjects\Database\Pdf"
         for x, y, z in os.walk(pdf_folder):
             print(x)
             print(y)
             print(z)
 
-    def user_pass(self):
+    def datetimestamp(self):
+        current_time = datetime.now()
+        modified_time = current_time.replace(microsecond=0)
+        my_time_format = "%d %h %Y %H:%M:%S"
+        converted_format_time = datetime.strftime(modified_time, my_time_format)
+        return converted_format_time
+
+    def pdf_creator(self):
         file_name = filedialog.askopenfilename()
         pdf_in_file = open(file_name, 'rb')
         inputpdf = PyPDF2.PdfFileReader(pdf_in_file)
         pages_no = inputpdf.numPages
+        pdfname = "Default.pdf"
         output = PyPDF2.PdfFileWriter()
         try:
             for i in range(pages_no):
@@ -50,55 +40,39 @@ class assign:
                 output.encrypt('password')
         except Exception as ex:
             print(ex)
-        with open("two4.pdf", "wb") as outputstream:
+        with open(r"C:\Users\Shreyas V Shetty\PycharmProjects\Database"
+                  r"\Pdf_Encrypted\Default.pdf", "wb") as outputstream:
             output.write(outputstream)
-class csvdata:
-    def csv(inputpdf, file_size):
-        current_time = datetime.now()
-        modified_time = current_time.replace(microsecond=0)
-        added_current_time = modified_time + timedelta(minutes=0)
-        print(added_current_time)
-        my_time_format = "%Y_%m_%d_%H_%M_%S"
-        converted_format_time = datetime.strftime(added_current_time, my_time_format)
-        old_name = r"C:\Users\Shreyas V Shetty\PycharmProjects\pythonProject\pythonProject\Pdf_Encryption\Pdf " \
-                   r"Encrypted files"
-        new_name = r"C:\Users\Shreyas V Shetty\PycharmProjects\pythonProject\pythonProject\Pdf_Encryption\Pdf" \
-                   r" Encrypted files" + converted_format_time + ".pdf"
-        os.rename(old_name, new_name)
-        source = r'C:\Users\Shreyas V Shetty\PycharmProjects\pythonProject\pythonProject\Pdf_Encryption\Pdf ' \
-                 r'Encrypted files'
-        dest = r'C:\Users\Shreyas V Shetty\PycharmProjects\pythonProject\pythonProject\Pdf_Encryption\Pdf ' \
-               r'Encrypted files'
-        for fname in os.listdir(source):
-            if fname.lower().endswith('.pdf'):
-                shutil.move(os.path.join(source, fname), dest)
-        f = open(r"C:\Users\Shreyas V Shetty\PycharmProjects\pythonProject\pythonProject\Pdf_Encryption\file.txt", 'r+')
-        w = csv.writer(f)
-        for path, dirs, files in os.walk(r"C:\Users\Shreyas V Shetty\PycharmProjects\pythonProject\pythonProject\Pdf"
-                                         r"_Encryption"):
-            for filename in files:
-                w.writerow(filename)
-                file_size = os.path.getsize(r"C:\Users\Shreyas V Shetty\PycharmProjects\pythonProject\pythonProject"
-                                            r"\Pdf_Encryption")
-                with open("file1.csv", "w") as file_obj:
-                    csv_file_obj = csv.writer(file_obj)
-                    csv_file_obj.writerow(["filename", file_size])
-                    csv_file_obj.writerows(["filename", file_size])
+        with open(pdfname, "wb") as outputstream:
+            output.write(outputstream)
+            self.datetimestamp()
 
-
-class datadb:
+    def filesizedata(self):
+        pdfname = "Default"
+        file_size = os.path.getsize(pdfname + ".pdf") / 1024
+        file_size = math.trunc(file_size)
+        return pdfname, file_size, self.datetimestamp()
 
     def database(self):
         connection = sqlite3.connect("PDF_Protection.db")
-        query = """CREATE TABLE Detail ("filename" text, "file_size in KB" integer)"""
-        execution = connection.execute(query)
-        db_data = execution.fetchall()
-        print(db_data)
+        cur = connection.cursor()
+        cur.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='Detail' ''')
+        if cur.fetchone()[0] == 1:
+            data = self.filesizedata()
+            query = """INSERT INTO Detail ("filename", "file size", "Time of encryption") VALUES (?,?,?)"""
+            execution = connection.execute(query, data)
+            db_data = execution.fetchall()
+            print(db_data)
+        else:
+            query = """CREATE TABLE Detail ("filename" text, "file size" integer, "Time of encryption" text)"""
+            execution = connection.execute(query)
+            db_data = execution.fetchall()
+            print(db_data)
         connection.commit()
         connection.close()
 
-    def my_db_exe(db="PDF_Protection.db"):
-        connection = sqlite3.connect(db)
+    def db_display(self):
+        connection = sqlite3.connect("PDF_Protection.db")
         query = """SELECT * FROM Detail"""
         execution = connection.execute(query)
         db_data = execution.fetchall()
@@ -106,29 +80,29 @@ class datadb:
         connection.commit()
         connection.close()
 
-    def database_update(inputpdf, my_time_format,password = 'password'):
-        file_size = os.path.getsize(inputpdf + ".pdf") / 1024
+    def file_csv(self):
+        pdfname = "Default"
+        file_size = os.path.getsize(pdfname + ".pdf") / 1024
         file_size = math.trunc(file_size)
-        if os.path.isfile("PDF_Protection.db") is True:
-            data = (inputpdf, file_size, my_time_format,password)
-            db = "PDF_Protection.db"
-            #query = """insert into Details values ("filename", "file_size in KB") VALUES ("two4.pdf",file_size) """
-            datadb.my_db_exe(query, db,data)
-            csvdata.csv(inputpdf, file_size)
+        if os.path.isfile("Pdf_data.csv") is False:
+            with open("Pdf_data.csv", "a", newline="") as file_obj:
+                obj = csv.writer(file_obj)
+                obj.writerow(["FileName", "File Size", "Time of encryption", "Email id", "Password"])
+                email = input("Enter your email ID: ")
+                password = input("Enter your email password: ")
+                obj.writerow(["filename", file_size, self.datetimestamp(), email, password])
         else:
-            datadb.database()
-            data = (inputpdf, file_size, my_time_format, password)
-            db = "PDF_Protection.db"
-            query = """insert into detail values (?,?,?,?);"""
-            datadb.my_db_exe(query, db, data)
-            csvdata.csv(inputpdf, file_size)
+            with open("Pdf_data.csv", "a", newline="") as file_obj:
+                obj = csv.writer(file_obj)
+                email = input("Enter your email ID: ")
+                password = input("Enter your email password: ")
+                obj.writerow([pdfname, file_size, self.datetimestamp(), email, password])
+        print("CSV file updated successfully")
 
 
-my_obj = assign()
+my_obj = Project()
 my_obj.pdf()
-my_obj.user_pass()
-my_obj1 = datadb()
-my_obj1.database()
-my_obj1.my_db_exe()
-my_obj1.database_update()
-
+my_obj.pdf_creator()
+my_obj.database()
+my_obj.db_display()
+my_obj.file_csv()
